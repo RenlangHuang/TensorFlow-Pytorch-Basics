918f1f679c25d60da1f11ca063dcfd9062559a6e
Renlang Huang <55647545+RenlangHuang@users.noreply.github.com>
2021-10-25 16:03:24 +0800

    Add files via upload

---
+++ lecture4_cnn/MNIST_CNN/p73_mnist_lenet.py
@@ -0,0 +1,132 @@
+import os
+#os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+#os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from tensorflow.keras import Model
+from tensorflow.keras.layers import (
+    Conv2D,
+    MaxPool2D,
+    Flatten,
+    Dense
+)
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print("x_train.shape", x_train.shape)
+print("y_train.shape", y_train.shape)
+print("x_test.shape", x_test.shape)
+print("y_test.shape", y_test.shape)
+print("categories:",set(y_train))
+np.set_printoptions(precision=2)
+
+# 可视化训练集输入
+sample = np.random.randint(0,x_train.shape[0])
+plt.imshow(x_train[sample], cmap='gray') # 绘制灰度图
+print('the label of the sample:',y_train[sample])
+plt.show()
+
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1) # 给数据增加一个维度，使数据和网络结构匹配
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+print("x_train.shape", x_train.shape)
+
+
+model = tf.keras.models.Sequential([
+    Conv2D(filters=6, kernel_size=(5, 5), activation='sigmoid'),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Conv2D(filters=16, kernel_size=(5, 5), activation='sigmoid'),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Flatten(),
+    Dense(120, activation='sigmoid'),
+    Dense(84, activation='sigmoid'),
+    Dense(10, activation='softmax')
+])
+class LeNet5(Model):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.c1 = Conv2D(filters=6, kernel_size=(5, 5), activation='sigmoid')
+        self.p1 = MaxPool2D(pool_size=(2, 2), strides=2)
+
+        self.c2 = Conv2D(filters=16, kernel_size=(5, 5), activation='sigmoid')
+        self.p2 = MaxPool2D(pool_size=(2, 2), strides=2)
+
+        self.flatten = Flatten()
+        self.f1 = Dense(120, activation='sigmoid')
+        self.f2 = Dense(84, activation='sigmoid')
+        self.f3 = Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.c1(x)
+        x = self.p1(x)
+
+        x = self.c2(x)
+        x = self.p2(x)
+
+        x = self.flatten(x)
+        x = self.f1(x)
+        x = self.f2(x)
+        y = self.f3(x)
+        return y
+
+model = LeNet5()
+
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.002),#'adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+checkpoint_save_path = "./checkpoint/LeNet5_mnist.ckpt"
+if os.path.exists(checkpoint_save_path + '.index'):
+    print('-------------load the model-----------------')
+    model.load_weights(checkpoint_save_path)
+
+'''cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_save_path,
+                                                 save_weights_only=True,
+                                                 save_best_only=True)
+
+history = model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1,
+                    callbacks=[cp_callback])
+model.summary()
+
+# print(model.trainable_variables)
+file = open('./weights_mnist_AlexNet.txt', 'w')
+for v in model.trainable_variables:
+    file.write(str(v.name) + '\n')
+    file.write(str(v.shape) + '\n')
+file.close()
+
+# 显示训练集和验证集的acc和loss曲线
+acc = history.history['sparse_categorical_accuracy']
+val_acc = history.history['val_sparse_categorical_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.subplot(1, 2, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()'''
+
+# prediction
+image = tf.reshape(x_train[sample],(1,28,28,1))
+image = tf.cast(image,dtype=tf.float32)
+print(image.shape)
+result = model.predict(image)
+print('softmax: ',result)
+print('softmax sum =',tf.reduce_sum(result))
+print('pred =',tf.argmax(result[0]).numpy(),', real =',y_train[sample])
+
+
+from p72_evaluation import evaluate
+training_accuracy = evaluate(model,x_train,y_train)
+validation_accuracy = evaluate(model,x_test,y_test)
+print('training_accuracy = %.4f, validation_accuracy = %.4f'%\
+    (training_accuracy,validation_accuracy))
