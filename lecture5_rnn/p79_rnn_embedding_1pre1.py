f0d75f4d717c34ae91f587919b243f326914ee1d
Renlang Huang <55647545+RenlangHuang@users.noreply.github.com>
2021-10-28 21:44:16 +0800

    Update p79_rnn_embedding_1pre1.py

--- lecture5_rnn/p79_rnn_embedding_1pre1.py
+++ lecture5_rnn/p79_rnn_embedding_1pre1.py
@@ -5,11 +5,22 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Dense, SimpleRNN, Embedding
 
 
-input_word = "abcde"
-w_to_id = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4}  # 单词映射到数值id的词典
+input_word = "abcdefghijklmnopqrstuvwxyz"
+w_to_id = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4,
+           'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9,
+           'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14,
+           'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19,
+           'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}  # 单词映射到数值id的词典
 
-x_train = [w_to_id['a'], w_to_id['b'], w_to_id['c'], w_to_id['d'], w_to_id['e']]
-y_train = [w_to_id['b'], w_to_id['c'], w_to_id['d'], w_to_id['e'], w_to_id['a']]
+training_set_scaled = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                       11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                       21, 22, 23, 24, 25]
+
+x_train, y_train = [], []
+
+for i in range(4, 26):
+    x_train.append(training_set_scaled[i - 4:i])
+    y_train.append(training_set_scaled[i])
 
 np.random.seed(7)
 np.random.shuffle(x_train)
@@ -19,23 +30,37 @@ tf.random.set_seed(7)
 
 # 使x_train符合Embedding输入要求：
 # [送入样本数， 循环核时间展开步数] ，
-# 此处整个数据集送入所以送入，
-# 送入样本数为len(x_train)；
-# 输入1个字母出结果，循环核时间展开步数为1。
-x_train = np.reshape(x_train, (len(x_train), 1))
+# 此处整个数据集送入
+# 输入4个字母出结果，循环核时间展开步数为4
+x_train = np.reshape(x_train, (len(x_train), 4))
 y_train = np.array(y_train)
 
-model = tf.keras.Sequential([
-    Embedding(5, 2),
-    SimpleRNN(3),
-    Dense(5, activation='softmax')
-])
+'''model = tf.keras.Sequential([
+    Embedding(26, 2),
+    SimpleRNN(10),
+    Dense(26, activation='softmax')
+])'''
+
+class RNN(tf.keras.Model):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.embedding = Embedding(26, 2)
+        self.rnn = SimpleRNN(10)
+        self.dense = Dense(26, activation='softmax')
+    def call(self, inputs):
+        y = self.embedding(inputs)
+        #print(y.shape)
+        y = self.rnn(y)
+        #print(y.shape)
+        return self.dense(y)
+
+model = RNN()
 
 model.compile(optimizer=tf.keras.optimizers.Adam(0.01),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
               metrics=['sparse_categorical_accuracy'])
 
-checkpoint_save_path = "./checkpoint/rnn_embedding_1pre1.ckpt"
+checkpoint_save_path = "./checkpoint/rnn_embedding_4pre1.ckpt"
 
 if os.path.exists(checkpoint_save_path + '.index'):
     print('-------------load the model-----------------')
@@ -44,13 +69,12 @@ if os.path.exists(checkpoint_save_path + '.index'):
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_save_path,
                                                  save_weights_only=True,
                                                  save_best_only=True,
-                                                 monitor='loss')
+                                                 monitor='loss')  # 由于fit没有给出测试集，不计算测试集准确率，根据loss，保存最优模型
 
-history = model.fit(x_train, y_train, batch_size=32, epochs=100, callbacks=[cp_callback])
+'''history = model.fit(x_train, y_train, batch_size=32, epochs=100, callbacks=[cp_callback])
 model.summary()
 
-# print(model.trainable_variables)
-file = open('./weights.txt', 'w')  # 参数提取
+file = open('./weights_rnn1_emb.txt', 'w')
 for v in model.trainable_variables:
     file.write(str(v.name) + '\n')
     file.write(str(v.shape) + '\n')
@@ -58,7 +82,6 @@ for v in model.trainable_variables:
 file.close()
 
 
-# 显示训练集和验证集的acc和loss曲线
 acc = history.history['sparse_categorical_accuracy']
 loss = history.history['loss']
 
@@ -71,14 +94,14 @@ plt.subplot(1, 2, 2)
 plt.plot(loss, label='Training Loss')
 plt.title('Training Loss')
 plt.legend()
-plt.show()
+plt.show()'''
 
 # predict
 preNum = int(input("input the number of test alphabet:"))
 for i in range(preNum):
     alphabet1 = input("input test alphabet:")
-    alphabet = [w_to_id[alphabet1]]
-    alphabet = np.reshape(alphabet, (1, 1))
+    alphabet = [w_to_id[a] for a in alphabet1]
+    alphabet = np.reshape(alphabet, (1, 4))
     result = model.predict(alphabet)
     pred = tf.argmax(result, axis=1)
     pred = int(pred)
