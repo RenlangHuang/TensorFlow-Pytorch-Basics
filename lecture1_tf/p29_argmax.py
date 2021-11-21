b8d142f5cdfd21152263636765aa52bbc3126c81
Renlang Huang <55647545+RenlangHuang@users.noreply.github.com>
2021-11-21 21:58:57 +0800

    Update p29_argmax.py

--- lecture1_tf/p29_argmax.py
+++ lecture1_tf/p29_argmax.py
@@ -13,3 +13,17 @@ test = tf.reshape(test,(-1,2))
 print("每一列的最大值的索引", tf.argmax(test, axis=0))
 
 # similarly, we can use tf.argmin
+
+
+# extract data from tensors with indices
+data = tf.constant([1, 2, 3, 4, 5, 6])
+indices = tf.constant([0, 1, 2, 1])
+print(tf.gather(data, indices))
+
+data = tf.constant([[1, 2], [3, 4], [5, 6]])
+indices = tf.constant([2, 0, 1])
+print(tf.gather(data, indices))
+
+indices = tf.constant([1, 0, 1])
+indices = list(enumerate(indices))
+print(tf.gather_nd(data, indices))
